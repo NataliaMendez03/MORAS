28e554894349e8dbd10c9404213b7f03d29e807b
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-04-13 00:52:39 -0500

    Dispatch empl

--- Sistema de Gestion Moras/Models/Dispatch.cs
+++ Sistema de Gestion Moras/Models/Dispatch.cs
@@ -7,13 +7,13 @@ namespace Sistema_de_Gestion_Moras.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdDispatch {  get; set; }
-        public required int IdEmployee { get; set; }
-        public required Employee Employee { get; set; }
-        public required int IdSalesDetails { get; set; }
+        public required int? IdEmployees { get; set; }
+        public required Employees Employee { get; set; }
+        public required int? IdSalesDetails { get; set; }
         public required SalesDetails SalesDetails { get; set; }
 
         public DateTime Date {  get; set; }
-        public required int IdTracking { get; set; }
+        public required int? IdTracking { get; set; }
         public required Tracking Tracking { get; set; }
 
     }
