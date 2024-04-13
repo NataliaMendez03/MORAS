bf608a786b540826af6e2393075b8b3e64d75e28
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-04-13 00:00:27 -0500

    dbContext cambiosss :D

--- Sistema de Gestion Moras/Context/morasdbContext.cs
+++ Sistema de Gestion Moras/Context/morasdbContext.cs
@@ -27,6 +27,22 @@ namespace Sistema_de_Gestion_Moras.Context
                 .HasKey(e => e.IdPerson);
             modelBuilder.Entity<Supplies>()
                 .HasKey(e => e.IdSupplies);
+
+            modelBuilder.Entity<PurchaseDetail>()
+                .HasKey(e => e.IdPurchaseDetail);
+            modelBuilder.Entity<Storage>()
+             .HasKey(e => e.IdStorage);
+            modelBuilder.Entity<Contact>()
+                .HasKey(e => e.IdContact);
+            modelBuilder.Entity<Employees>()
+                .HasKey(e => e.IdEmployees);
+            modelBuilder.Entity<Post>()
+                .HasKey(e => e.IdPost);
+            modelBuilder.Entity<State>()
+                .HasKey(e => e.IdState);
+            modelBuilder.Entity<IdentificationType>()
+                .HasKey(e => e.IdIdentificationType);
+
             //HasNoKey()
             foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
             .SelectMany(e => e.GetForeignKeys()))
@@ -42,5 +58,13 @@ namespace Sistema_de_Gestion_Moras.Context
         public DbSet<Harvests> Harvests { get; set; }
         public DbSet<Person> Person { get; set; }
         public DbSet<Supplies> Supplies { get; set; }
+
+        public DbSet<PurchaseDetail> PurchaseDetail { get; set; }
+        public DbSet<Storage> Storage { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+        public DbSet<Employees> Employees { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<State> State { get; set; }
+        public DbSet<IdentificationType> IdentificationType { get; set; }
     }
 }
