5c94f6148d6ee16b0090044ec8ecfcad9cecdfcb
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-04-13 00:56:09 -0500

    employees 3

--- Sistema de Gestion Moras/Models/Harvests.cs
+++ Sistema de Gestion Moras/Models/Harvests.cs
@@ -9,9 +9,9 @@ namespace Sistema_de_Gestion_Moras.Models
         public int IdHarvests { get; set; }
         public DateTime? HarvestDate { get; set; }
         public string? HarvestAmount { get; set; }
-        public required int Idemployee { get; set; }
-        public required Employee Employee { get; set; }
-        public required int IdQuality { get; set; }
+        public required int? Idemployees { get; set; }
+        public required Employees Employees { get; set; }
+        public required int? IdQuality { get; set; }
         public required Quality Quality { get; set; }
 
     }
