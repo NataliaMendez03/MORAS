7052b6f379ebfdb30c8262b343ac5df7f1ab9e0f
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-04-19 17:02:38 -0500

    Cargo rep y serv

---
+++ Sistema de Gestion Moras/Repositories/PostRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema_de_Gestion_Moras.Context;
+using Sistema_de_Gestion_Moras.Models;
+using System.Net;
+
+namespace Sistema_de_Gestion_Moras.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<List<Post>> GetAll();
+        Task<Post> GetPost(int idPost);
+        Task<Post> CreatePost(string namePost);
+        Task<Post> UpdatePost(Post post);
+        Task<Post> DeletePost(Post post);
+    }
+    public class PostRepository : IPostRepository
+    {
+        private readonly berriesdbContext _db;
+        public PostRepository(berriesdbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Post> CreatePost(string namePost)
+        {
+            Post newPost = new Post
+            {
+                NamePost = namePost,
+                StateDelete = false,
+                CreatedDate = null
+            };
+
+            await _db.Post.AddAsync(newPost);
+            _db.SaveChanges();
+
+            return newPost;
+        }
+
+        public async Task<Post> DeletePost(Post post)
+        {
+            _db.Post.Attach(post); //Llamamos la actualizacion
+            _db.Entry(post).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return post;
+        }
+
+        public async Task<List<Post>> GetAll()
+        {
+            return await _db.Post.ToListAsync();
+        }
+
+        public async Task<Post> GetPost(int idPost)
+        {
+            return await _db.Post.FirstOrDefaultAsync(u => u.IdPost == idPost);
+        }
+
+        public async Task<Post> UpdatePost(Post post)
+        {
+            Post PostUpdate = await _db.Post.FindAsync(post.IdPost);
+            if (PostUpdate != null)
+            {
+                //?? ConsultUpdate.IdConsult = idConsult;
+                PostUpdate.NamePost = post.NamePost;
+
+
+                await _db.SaveChangesAsync();
+            }
+
+            return PostUpdate;
+        }
+    }
+}
---
+++ Sistema de Gestion Moras/Services/PostService.cs
@@ -0,0 +1,64 @@
+﻿using Sistema_de_Gestion_Moras.Models;
+using Sistema_de_Gestion_Moras.Repositories;
+using System.Net;
+
+namespace Sistema_de_Gestion_Moras.Services
+{
+    public interface IPostService
+    {
+        Task<List<Post>> GetAll();
+        Task<Post> GetPost(int idPost);
+        Task<Post> CreatePost(string namePost);
+        Task<Post> UpdatePost(int idPost, string? namePost = null);
+        Task<Post> DeletePost(int idPost);
+    }
+    public class PostService : IPostService
+    {
+        public readonly IPostRepository _postRepository;
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public async Task<Post> CreatePost(string namePost)
+        {
+            return await _postRepository.CreatePost(namePost);
+        }
+
+        public async Task<Post> DeletePost(int idPost)
+        {
+            // comprobar si existe
+            Post PostToDelete = await _postRepository.GetPost(idPost);
+            if (PostToDelete == null)
+            {
+                throw new Exception($"This Post with the Id {idPost} don´t exist. ");
+            }
+            PostToDelete.StateDelete = true;
+            PostToDelete.CreatedDate = DateTime.Now;
+            return await _postRepository.DeletePost(PostToDelete);
+        }
+
+        public async Task<List<Post>> GetAll()
+        {
+            return await _postRepository.GetAll();
+        }
+
+        public async Task<Post> GetPost(int idPost)
+        {
+            return await _postRepository.GetPost(idPost);
+        }
+
+        public async Task<Post> UpdatePost(int idPost, string? namePost = null)
+        {
+            Post newPost = await _postRepository.GetPost(idPost);
+            if (newPost != null)
+            {
+                if (namePost != null)
+                {
+                    newPost.NamePost = (string)namePost;
+                }
+                return await _postRepository.UpdatePost(newPost);
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
