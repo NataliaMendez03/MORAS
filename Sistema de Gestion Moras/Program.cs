29212f3e96a136a9525dad35bc2199923d8eec2c
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-04-19 23:11:23 -0500

    Program builder services.

--- Sistema de Gestion Moras/Program.cs
+++ Sistema de Gestion Moras/Program.cs
@@ -1,6 +1,8 @@
 using Sistema_de_Gestion_Moras.Context;
 using Sistema_de_Gestion_Moras.Models;
 using Microsoft.EntityFrameworkCore;
+using Sistema_de_Gestion_Moras.Repositories;
+using Sistema_de_Gestion_Moras.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,39 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region AppRepositories
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IDispatchRepository, DispatchRepository>();
+builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
+builder.Services.AddScoped<IHarvestsRepository, HarvestsRepository>();
+builder.Services.AddScoped<IIdentificationTypeRepository, IdentificationTypeRepository>();
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IPurchaseDetailRepository, PurchaseDetailRepository>();
+builder.Services.AddScoped<ISalesDetailsRepository, SalesDetailsRepository>();
+builder.Services.AddScoped<IStorageRepository, StorageRepository>();
+builder.Services.AddScoped<ISuppliesRepository, SuppliesRepository>();
+builder.Services.AddScoped<ITrackingRepository, TrackingRepository>();
+#endregion
+
+#region AppServices
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IContactService, ContactService>();
+builder.Services.AddScoped<IDispatchService, DispatchService>();
+builder.Services.AddScoped<IEmployeesService, EmployeesService>();
+builder.Services.AddScoped<IHarvestsService, HarvestsService>();
+builder.Services.AddScoped<IIdentificationTypeService, IdentificationTypeService>();
+builder.Services.AddScoped<IPersonService, PersonService>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IPurchaseDetailService, PurchaseDetailService>();
+builder.Services.AddScoped<ISalesDetailsService, SalesDetailsService>();
+builder.Services.AddScoped<IStorageService, StorageService>();
+builder.Services.AddScoped<ISuppliesService, SuppliesService>();
+builder.Services.AddScoped<ITrackingService, TrackingService>();
+#endregion
 
 var app = builder.Build();
 
