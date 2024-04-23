7ab98c720f4635953ec3c98bd81cdc5dda7eea9d
JULIAN ANDRES ALMARIO BAENA <jaalmario@ucundinamarca.edu.co>
2024-04-23 10:09:43 -0500

    Controladores, falta comprobar

---
+++ Sistema de Gestion Moras/Controllers/HarvestsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_de_Gestion_Moras.Models;
+using Sistema_de_Gestion_Moras.Repositories;
+using Sistema_de_Gestion_Moras.Services;
+
+namespace Sistema_de_Gestion_Moras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HarvestsController : ControllerBase
+    {
+        public readonly IHarvestsService _harvestsService;
+        public HarvestsController(IHarvestsService harvestsService)
+        {
+            _harvestsService = harvestsService;
+        }
+        // GET: api/<HarvestsController>
+        [HttpGet]
+        public async Task<ActionResult<List<Harvests>>> GetAllHarvests()
+        {
+            return Ok(await _harvestsService.GetAll());
+        }
+        // GET api/<HarvestsController>/5
+        [HttpGet("{idHarvests}")]
+        public async Task<ActionResult<Harvests>> GetHarvests(int idHarvests)
+        {
+            var Harvests = await _harvestsService.GetHarvests(idHarvests);
+            if (Harvests == null)
+            {
+                return BadRequest("Harvests not found");
+            }
+            return Ok(Harvests);
+        }
+        // POST: api/Harvests
+        [HttpPost("{harvestDate,harvestAmount,idemployees, idQuality }")]
+        public async Task<ActionResult<Harvests>> PostHarvests(DateTime harvestDate, string harvestAmount, int idemployees, int idQuality)
+        {
+            var HarvestsToPut = _harvestsService.CreateHarvests(harvestDate, harvestAmount, idemployees, idQuality);
+
+            if (HarvestsToPut != null)
+            {
+                //return CreatedAtAction(nameof(GetEps), new { id = idEps }, epsToPut);
+                return Ok(HarvestsToPut);
+            }
+            else
+            {
+                return BadRequest("Error when inserting into the database");
+            }
+
+
+        }
+        // PUT: api/Harvests/5
+        [HttpPut("Update/{idHarvests}")]
+        public async Task<ActionResult<Harvests>> PutHarvests(int idHarvests, DateTime harvestDate, string harvestAmount, int idemployees, int idQuality)
+        {
+            var HarvestsToPut = _harvestsService.UpdateHarvests(idHarvests,harvestDate, harvestAmount, idemployees, idQuality);
+
+            if (HarvestsToPut != null)
+            {
+                return Ok(HarvestsToPut);
+            }
+            else
+            {
+                return BadRequest("Error updating the database");
+            }
+        }
+        // Delete: api/Harvests/5
+        [HttpPut("Delete/{idHarvests}")]
+        public async Task<ActionResult<Harvests>> DeleteHarvests(int idHarvests)
+        {
+            var HarvestsToDelete = _harvestsService.DeleteHarvests(idHarvests);
+
+            if (HarvestsToDelete != null)
+            {
+                return Ok(HarvestsToDelete);
+            }
+            else
+            {
+                return BadRequest("Error updating the database");
+            }
+        }
+    }
+}
---
+++ Sistema de Gestion Moras/Controllers/SuppliesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_de_Gestion_Moras.Models;
+using Sistema_de_Gestion_Moras.Services;
+
+namespace Sistema_de_Gestion_Moras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliesController : ControllerBase
+    {
+        public readonly ISuppliesService _suppliesService;
+        public SuppliesController(ISuppliesService suppliesService)
+        {
+            _suppliesService = suppliesService;
+        }
+        // GET: api/<SuppliesController>
+        [HttpGet]
+        public async Task<ActionResult<List<Supplies>>> GetAllSupplies()
+        {
+            return Ok(await _suppliesService.GetAll());
+        }
+        // GET api/<SuppliesController>/5
+        [HttpGet("{idSupplies}")]
+        public async Task<ActionResult<Supplies>> GetSupplies(int idSupplies)
+        {
+            var supplies = await _suppliesService.GetSupplies(idSupplies);
+            if (supplies == null)
+            {
+                return BadRequest("Supplies not found");
+            }
+            return Ok(supplies);
+        }
+        // POST: api/Supplies
+        [HttpPost("{idSupplies }")]
+        public async Task<ActionResult<Supplies>> PostSupplies(string nameSupplies)
+        {
+            var SuppliesToPut = _suppliesService.CreateSupplies(nameSupplies);
+
+            if (SuppliesToPut != null)
+            {
+                //return CreatedAtAction(nameof(GetEps), new { id = idEps }, epsToPut);
+                return Ok(SuppliesToPut);
+            }
+            else
+            {
+                return BadRequest("Error when inserting into the database");
+            }
+
+
+        }
+        // PUT: api/Supplies/5
+        [HttpPut("Update/{idSupplies}")]
+        public async Task<ActionResult<Supplies>> PutSupplies(int idSupplies, string nameSupplies)
+        {
+            var SuppliesToPut = _suppliesService.UpdateSupplies(idSupplies, nameSupplies);
+
+            if (SuppliesToPut != null)
+            {
+                return Ok(SuppliesToPut);
+            }
+            else
+            {
+                return BadRequest("Error updating the database");
+            }
+        }
+        // Delete: api/Supplies/5
+        [HttpPut("Delete/{idSupplies}")]
+        public async Task<ActionResult<Supplies>> DeleteSupplies(int idSupplies)
+        {
+            var SuppliesToDelete = _suppliesService.DeleteSupplies(idSupplies);
+
+            if (SuppliesToDelete != null)
+            {
+                return Ok(SuppliesToDelete);
+            }
+            else
+            {
+                return BadRequest("Error updating the database");
+            }
+        }
+    }
+}
