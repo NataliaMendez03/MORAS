4e355a5d1ea17a87d446c019857020956ddfc03f
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-05-14 23:54:10 -0500

    YA ENCRIPTA QUE LINDO

--- Sistema de Gestion Moras/Services/LoginService.cs
+++ Sistema de Gestion Moras/Services/LoginService.cs
@@ -2,6 +2,9 @@
 using Sistema_de_Gestion_Moras.Models;
 using Sistema_de_Gestion_Moras.Repositories;
 using System.Net;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Sistema_de_Gestion_Moras.Services
 {
@@ -9,11 +12,12 @@ namespace Sistema_de_Gestion_Moras.Services
     {
         Task<List<Login>> GetAll();
         Task<Login> GetLogin(int idLogin);
+        Task<int?> GetIdByUsername(string username);
         Task<Login> CreateLogin(string userName, string password, string email);
-        Task<Login> UpdateLogin(int idLogin, string? userName = null, string? password =null, string? email=null);
+        Task<Login> UpdateLogin(int idLogin, string? userName = null, string? password = null, string? email = null);
         Task<Login> DeleteLogin(int idLogin);
-        Task<Login> Login(string userName, string password);
-
+        Task<bool> Authentication(string userName, string password);
+        string EncriptarClave(string password);
     }
 
     public class LoginService : ILoginService
@@ -26,6 +30,7 @@ namespace Sistema_de_Gestion_Moras.Services
 
         public async Task<Login> CreateLogin(string userName, string password, string email)
         {
+            password = EncriptarClave(password);
             return await _loginRepository.CreateLogin(userName, password, email);
             throw new NotImplementedException();
         }
@@ -49,16 +54,40 @@ namespace Sistema_de_Gestion_Moras.Services
             throw new NotImplementedException();
         }
 
+        public async Task<int?> GetIdByUsername(string username)
+        {
+            return await _loginRepository.GetIdByUsername(username);
+            throw new NotImplementedException();
+        }
+
         public async Task<Login> GetLogin(int idLogin)
         {
             return await _loginRepository.GetLogin(idLogin);
             throw new NotImplementedException();
         }
 
-//AUTENTICACION----------------------------------------------------------------------------
-        public async Task<Login> Login(string userName, string password)
+
+// ENCRIPTAR CONTRASEÑA ----------------------------------------------------------------------------
+        public string EncriptarClave(string password)
         {
-            return await _loginRepository.Login(userName, password);
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(password));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+//AUTENTICACION----------------------------------------------------------------------------
+        public async Task<bool> Authentication(string userName, string password)
+        {
+            var user = await _loginRepository.AuthUser(userName);
+            string hashedPassword = EncriptarClave(password);
+
+            if (user != null && (user.Password == hashedPassword))
+                return true;
+            return false;
+            //return await _loginRepository.AuthUsername(userName, password);
             throw new NotImplementedException();
 
         }
@@ -81,10 +110,14 @@ namespace Sistema_de_Gestion_Moras.Services
                 {
                     newLogin.UserName = (string)email;
                 }
-                
+
                 return await _loginRepository.UpdateLogin(newLogin);
             }
             throw new NotImplementedException();
         }
+
+
     }
+
 }
+
