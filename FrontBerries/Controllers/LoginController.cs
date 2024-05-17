3116aef853ab29e454ccaa34fe4b0ce569455f72
NataliaMendez03 <166230816+NataliaMendez03@users.noreply.github.com>
2024-05-17 00:19:29 -0500

    Vista User

---
+++ FrontBerries/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using FrontBerries.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FrontBerries.Controllers
+{
+    public class LoginController : Controller
+    {
+        Uri baseAddress = new Uri("http://berriessystemmanagement.somee.com/api");
+        private readonly HttpClient _client;
+
+        public LoginController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        [HttpGet]
+        public IActionResult LoginGet()
+        {
+            List<LoginViewModel> Loginlist = new List<LoginViewModel>();
+            HttpResponseMessage respone = _client.GetAsync(_client.BaseAddress + "/Login").Result;
+            if(respone.IsSuccessStatusCode)
+            {
+                string data = respone.Content.ReadAsStringAsync().Result;
+                Loginlist = JsonConvert.DeserializeObject<List<LoginViewModel>>(data);
+            }
+            return View(Loginlist);
+        }
+    }
+}
---
+++ FrontBerries/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrontBerries.Models
+{
+    public class LoginViewModel
+    {
+        [DisplayName("Id")]
+        public int IdLogin { get; set; }
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        [DisplayName("Register Date")]
+        public DateTime RegisterDate { get; set; }
+    }
+}
