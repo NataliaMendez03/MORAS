61d480f9ef0d43fc3b470d21f8dd52a392ae1f9f
Maria Camila Tovar Acosta <mctovar@ucundinamarca.edu.co>
2024-05-18 20:21:20 -0500

    layout y register des

---
+++ FrontBerries/ViewModels/SystemLoginVM.cs
@@ -0,0 +1,24 @@
+﻿using Sistema_de_Gestion_Moras.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FrontBerries.ViewModels
+{
+    public class SystemLoginVM
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public int IdPerson { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Email { get; set; }
+        public int Phone { get; set; }
+        public int TypeIdentification { get; set; }
+        public int NumberIdentification { get; set; }
+        public int Address { get; set; }
+        public int City { get; set; }
+
+    }
+}
