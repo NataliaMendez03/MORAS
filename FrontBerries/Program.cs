48469ef2027b4a579bc5941c7ff1ede3947f21eb
NataliaMendez03 <166230816+NataliaMendez03@users.noreply.github.com>
2024-05-21 01:59:52 -0500

    Hello

--- FrontBerries/Program.cs
+++ FrontBerries/Program.cs
@@ -17,13 +17,14 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     {
         options.LoginPath = "/Main/MainPage";
         options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error"); 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
